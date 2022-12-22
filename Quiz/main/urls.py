5c0172499d00d3d03d0b8fbd0e50101f7ffee1aa
DujeVidas <dujevidas123@gmail.com>
2022-12-22 17:16:34 +0100

    Izradjeni bazicni generički viewovi; treba ih jos malo poljepšat i napravit da rade sa različitim relationshipima modela

--- Quiz/main/urls.py
+++ Quiz/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from main.views import *
 
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('kategorije', KategorijeList.as_view()),
+    path('kvizovi', KvizoviList.as_view()),
+    path('pitanja', PitanjaList.as_view()),
+    path('opisi', OpisiList.as_view())
 ]
--- Quiz/main/views.py
+++ Quiz/main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
+from main.models import *
 
 ## Create your views here.
 def homepage(request):
     return HttpResponse('Welcome to homepage! <strong>#samoOIRI</strong>')
     # primjetiti korištenje HTML-a
+
+class KategorijeList(ListView):
+    model = Kategorija
+
+class KvizoviList(ListView):
+    model = Kviz
+
+
+class PitanjaList(ListView):
+    model = Pitanje
+
+class OpisiList(ListView):
+    model = OpisPitanja
